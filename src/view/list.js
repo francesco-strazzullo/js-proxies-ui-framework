a5d9e2aa6a3cd0a6c655604d5f5dac5d5ca0a757
francesco-strazzullo <me@francescostrazzullo.info>
2016-10-21 14:06:10 +0200

    fixed delete

--- src/view/list.js
+++ src/view/list.js
@@ -2,8 +2,7 @@ import { h, diff } from 'virtual-dom';
 
 export default (state) => {
     const onRemoveClick = (index) => {
-        state.todos.splice(index,1);
-        state.todos = [...state.todos];
+        state.todos = [...state.todos.slice(0, index), ...state.todos.slice(index + 1)];
     };
 
     const createDeleteButton = (index) => h('button',{
